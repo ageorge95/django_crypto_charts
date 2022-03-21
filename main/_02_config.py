9ace9441e61a2d77be2bb8a62b8479185b68ed1b
George Daniel Arteni <arteni.george.daniel@gmail.com>
2022-03-21 19:07:13 +0200

    updated the config fixed citex SIT 6months formatted the spacing, so that everything is aligned :)

--- main/_02_config.py
+++ main/_02_config.py
@@ -9,53 +9,53 @@ pairs_to_show = {
                           'method_args': {'pair': 'btc_usdt',
                                           'period_s': '1day'},
                           'title': 'BTC long __ btc_usdt __ 10 MONTHS'}],
-                 'HDD': [
+                'HDD': [
+                       {'platform_link': 'https://www.xt.com/tradePro/hdd_usdt',
+                        'platform': 'XT',
+                        'method_args': {'pair': 'hdd_usdt',
+                                        'period_s': '1min'},
+                        'title': 'HDD short __ hdd_usdt __ 6 HOURS'},
                         {'platform_link': 'https://www.xt.com/tradePro/hdd_usdt',
                          'platform': 'XT',
                          'method_args': {'pair': 'hdd_usdt',
-                                         'period_s': '1min'},
-                         'title': 'HDD short __ hdd_usdt __ 6 HOURS'},
-                         {'platform_link': 'https://www.xt.com/tradePro/hdd_usdt',
-                          'platform': 'XT',
-                          'method_args': {'pair': 'hdd_usdt',
-                                          'period_s': '1day'},
-                          'title': 'HDD long __ hdd_usdt __ 10 MONTHS'},
-                         ],
-                 'SPARE': [
-                         {'platform_link': 'https://www.xt.com/tradePro/spare_usdt',
+                                         'period_s': '1day'},
+                         'title': 'HDD long __ hdd_usdt __ 10 MONTHS'},
+                        ],
+                'SPARE': [
+                        {'platform_link': 'https://www.xt.com/tradePro/spare_usdt',
+                        'platform': 'XT',
+                        'method_args': {'pair': 'spare_usdt',
+                                        'period_s': '1min'},
+                        'title': 'SPARE short __ spare_usdt __ 6 HOURS'},
+                        {'platform_link': 'https://www.xt.com/tradePro/spare_usdt',
                          'platform': 'XT',
                          'method_args': {'pair': 'spare_usdt',
-                                         'period_s': '1min'},
-                         'title': 'SPARE short __ spare_usdt __ 6 HOURS'},
-                         {'platform_link': 'https://www.xt.com/tradePro/spare_usdt',
-                          'platform': 'XT',
-                          'method_args': {'pair': 'spare_usdt',
-                                          'period_s': '1day'},
-                          'title': 'SPARE long __ spare_usdt __ 10 MONTHS'}
-                 ],
-                 'XCH': [
+                                         'period_s': '1day'},
+                         'title': 'SPARE long __ spare_usdt __ 10 MONTHS'}
+                ],
+                'XCH': [
+                       {'platform_link': 'https://www.xt.com/tradePro/xch_usdt',
+                        'platform': 'XT',
+                        'method_args': {'pair': 'xch_usdt',
+                                        'period_s': '1min'},
+                        'title': 'XCH short __ xch_usdt __ 6 HOURS'},
                         {'platform_link': 'https://www.xt.com/tradePro/xch_usdt',
                          'platform': 'XT',
                          'method_args': {'pair': 'xch_usdt',
-                                         'period_s': '1min'},
-                         'title': 'XCH short __ xch_usdt __ 6 HOURS'},
-                         {'platform_link': 'https://www.xt.com/tradePro/xch_usdt',
-                          'platform': 'XT',
-                          'method_args': {'pair': 'xch_usdt',
-                                          'period_s': '1day'},
-                          'title': 'XCH long __ xch_usdt __ 10 MONTHS'}
-                 ],
-                 'SIT': [
+                                         'period_s': '1day'},
+                         'title': 'XCH long __ xch_usdt __ 10 MONTHS'}
+                ],
+                'SIT': [
+                       {'platform_link': 'https://trade.citex.co.kr/trade/SIT_USDT',
+                        'platform': 'CITEX',
+                        'method_args': {'pair': 'sit_usdt',
+                                        'period_m': 3, # 1 does not work as of 21 Mar 2022
+                                        'size': 120},
+                        'title': 'SIT short __ sit_usdt __ 6 HOURS'},
                         {'platform_link': 'https://trade.citex.co.kr/trade/SIT_USDT',
                          'platform': 'CITEX',
                          'method_args': {'pair': 'sit_usdt',
-                                         'period_m': 1,
-                                         'size': 360},
-                         'title': 'SIT short __ sit_usdt __ 6 HOURS'},
-                         {'platform_link': 'https://trade.citex.co.kr/trade/SIT_USDT',
-                          'platform': 'CITEX',
-                          'method_args': {'pair': 'sit_usdt',
-                                          'period_m': 1440,
-                                          'size': 300},
-                          'title': 'SIT long __ sit_usdt __ 10 MONTHS'}
-                 ]}
+                                         'period_m': 1440,
+                                         'size': 300},
+                         'title': 'SIT long __ sit_usdt __ 10 MONTHS'}
+                ]}
